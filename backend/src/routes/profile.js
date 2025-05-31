8b95f8ab022a7054b0e6a08498d1f5711c675fc1
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-06-01 00:59:25 +0530

    remove the profileOwner from profile update

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -15,7 +15,7 @@ router.patch("/profile/data",validateTokenAndFindUser,wrapasync(updateUserProfil
 router.get("/profile",validateTokenAndFindUser,wrapasync(getUserAndProfile))
 router.get("/profiles",wrapasync(getUserAndProfileAll))
 router.get("/getuserbasedonusername",wrapasync(getuserbasedonusername))
-router.patch("/profiles/data",validateTokenAndFindUser,profileOwner,wrapasync(updateProfileData))
+router.patch("/profiles/data",validateTokenAndFindUser,wrapasync(updateProfileData))
 router.post("/connections",validateTokenAndFindUser,wrapasync(sendConnectionRequest));
 router.get("/connections/sent",validateTokenAndFindUser,wrapasync(getMyConnectionRequest))
 router.get("/connections/received",validateTokenAndFindUser,wrapasync(whatAreMyConnections))
