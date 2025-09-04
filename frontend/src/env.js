350e989c9df5603886fe6fd9bfae3e61c9d0471f
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-09-04 19:32:14 +0530

    add backend url

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ?"http://localhost:8080":"https://linkedin-backend-m636.onrender.com"
+  ?"http://ec2-13-49-80-235.eu-north-1.compute.amazonaws.com:8080":"http://localhost:8080";
   
 
 export default server;
